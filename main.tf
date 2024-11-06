c6b8880d0d79af35238b732c915b93fb31065ea5
Colin Lacy <colin.lacy@meraki.net>
2024-11-06 10:19:37 -0500

    adds incomplete onion resources

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+########################################################
+
+# GARLIC RESOURCES
+
+########################################################
 resource "github_repository" "garlic" {
   name        = "garlic"
   description = "Next-level awesome for your codebase"
@@ -19,6 +24,43 @@ resource "github_branch_default" "garlic_kitchen" {
 resource "github_branch_protection" "garlic_kitchen" {
   pattern           = github_branch_default.garlic_kitchen.branch
   repository_id       = github_repository.garlic.id
+  required_status_checks {
+    strict   = true
+    contexts = ["continuous-integration"]
+  }
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    require_code_owner_reviews = true
+  }
+  enforce_admins = true
+}
+
+########################################################
+
+# ONION RESOURCES
+
+########################################################
+resource "github_repository" "onion" {
+  name        = "onion"
+  description = "Gives garlic that extra boost!"
+  auto_init = true
+  visibility = "public"
+  archive_on_destroy = true
+}
+
+resource "github_branch" "onion_kitchen" {
+  branch     = "kitchen"
+  repository = github_repository.onion.name
+}
+
+resource "github_branch_default" "onion_kitchen" {
+  branch     = github_branch.onion_kitchen.branch
+  repository = github_repository.onion.name
+}
+
+resource "github_branch_protection" "onion_kitchen" {
+  pattern           = github_branch_default.onion_kitchen.branch
+  repository_id       = github_repository.onion.id
   required_status_checks {
     strict   = true
     contexts = ["continuous-integration"]
