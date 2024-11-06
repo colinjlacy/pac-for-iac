3805fe5a6797d20dbfa95ad0dbcd41ee9e372c48
Colin Lacy <colin.lacy@meraki.net>
2024-11-06 10:06:14 -0500

    adds simple resource definitions, no modules

--- main.tf
+++ main.tf
@@ -1,17 +1,31 @@
-module "garlic" {
-  source      = "./modules/github"
+resource "github_repository" "garlic" {
   name        = "garlic"
   description = "Next-level awesome for your codebase"
-  team_name   = "cooks"
-  branch_name = "kitchen"
-  members = ["colinjlacy"]
+  auto_init = true
+  visibility = "public"
+  archive_on_destroy = true
 }
 
-module "onion" {
-  source      = "./modules/github"
-  name        = "onion-cli"
-  description = "Gives garlic that extra boost!"
-  team_name   = "chefs"
-  branch_name = "kitchen"
-  members = ["colinjlacy"]
+resource "github_branch" "garlic_kitchen" {
+  branch     = "kitchen"
+  repository = github_repository.garlic.name
+}
+
+resource "github_branch_default" "garlic_kitchen" {
+  branch     = github_branch.garlic_kitchen.branch
+  repository = github_repository.garlic.name
+}
+
+resource "github_branch_protection" "garlic_kitchen" {
+  pattern           = github_branch_default.garlic_kitchen.branch
+  repository_id       = github_repository.garlic.id
+  required_status_checks {
+    strict   = true
+    contexts = ["continuous-integration"]
+  }
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    require_code_owner_reviews = true
+  }
+  enforce_admins = true
 }
